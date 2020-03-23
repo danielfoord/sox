69054e12366c8562a229b6e3cc14474f21f2eeb1
Daniel Foord <danielfoord@users.noreply.github.com>
2020-03-23 23:37:26 +0200

    Added benchmark project

---
+++ Sox.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using Sox.Benchmarks.Websocket.Rfc6455.Framing;
+
+namespace Sox.Benchmarks
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BenchmarkRunner.Run<FrameBench>();
+        }
+    }
+}

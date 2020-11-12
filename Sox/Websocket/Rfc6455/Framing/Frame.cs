ebd020cbf3c669f687da00a9c4dc0dc49e217b52
Daniel Foord <danielfoord@users.noreply.github.com>
2020-11-12 18:46:48 +0200

    Protocol bug fix

--- Sox/Websocket/Rfc6455/Framing/Frame.cs
+++ Sox/Websocket/Rfc6455/Framing/Frame.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Threading.Tasks;
 
 /*
-  https://tools.ietf.org/html/rfc6455
+  https://tools.ietf.org/html/rfc6455#section-5.2
 
   0                   1                   2                   3
   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
